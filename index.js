8b66eb513375c12a883b6cc56f6859a956efb2d9
David Bernhard <davidbernhard@live.com>
2018-11-01 10:28:11 +0000

    functionality close navbar button

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const navLabel = document.querySelector(".nav-toggle");
 const navigation = document.querySelector(".navigation");
+const closeNav = document.querySelector(".close-nav");
 
 navLabel.addEventListener("click", showNav);
+closeNav.addEventListener("click", hideNav);
 
 function showNav() {
   navigation.style.transform = "translateX(0)";
+}
+
+function hideNav() {
+  navigation.style.transform = "translateX(-380px)";
 }
