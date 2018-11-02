c6e7f3c8fd7d476345454d4c61e35bcd0931fc6a
David Bernhard <davidbernhard@live.com>
2018-11-02 16:33:12 +0000

    start colour change functionality

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+//================= SELECTORS ================
+
 const navLabel = document.querySelector(".nav-toggle");
 const navigation = document.querySelector(".navigation");
 const closeNav = document.querySelector(".close-nav");
+const hamburgerDivs = document.querySelectorAll(".header__nav-icon--bar");
+const siteTitle = document.querySelector(".header__title");
+const currentSeason = document.querySelector(".season-display");
+
+//================== COLOURS ==================
+
+const orange = "#dd693f";
+const pink = "#54451";
+const darkBlue = "#303744";
+const yellow = "#d9e126";
+const red = "#d9455c";
+
+//=============== NAV FUNCTIONALITY ================
 
 navLabel.addEventListener("click", showNav);
 closeNav.addEventListener("click", hideNav);
@@ -10,5 +25,21 @@ function showNav() {
 }
 
 function hideNav() {
-  navigation.style.transform = "translateX(-380px)";
+  navigation.style.transform = "translateX(-450px)";
 }
+
+// ============ HEADER COLOUR CHANGES ==============  
+
+// hamburgerDivs.forEach(function(div){
+// 	div.style.backgroundColor = "red";
+// });
+
+function scrollCondition() {
+
+}
+
+function changeColour(colour) {
+
+}
+
+window.addEventListener("scroll", scrollCondition);
